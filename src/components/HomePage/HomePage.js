40b4644e7353cf4fb12a43386db0782053abaead
Anton Shumeiko <49491454+strang999@users.noreply.github.com>
2020-12-03 01:39:55 +0200

    Initial

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Weather app</h2>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
